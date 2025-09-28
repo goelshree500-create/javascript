ad28f4e6e0e3b26d9e1275ac8be660e0194dc552
g_shree <goelshree500@gmail.com>
2025-09-28 02:35:24 +0000

    variable

---
+++ basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=1234
+let accountName="shree"
+var accountage=23
+accountCity="jaipur"
+let accountState
+// console.log(accountId)
+// accountId=908    //not allowed
+accountName="abs"
+accountAge=90
+accountCity="mumbai"
+console.table([accountId,accountName,accountAge,accountCity,accountState])
