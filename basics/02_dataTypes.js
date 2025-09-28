f7292a3d603861e38172c88db42df2543f8d3119
g_shree <goelshree500@gmail.com>
2025-09-28 02:35:49 +0000

    datatype

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  //treat all JS code as newer version
+
+// alert(hello)  //we are using node.js not browser
+
+
+let name="shree"   //string datatyep
+let age=18  //number datatype
+
+
+//number=>2 to pow 53(range)
+//bigint
+//string=>""
+//boolean => true/false
+//symbol => unique
+//null =>standalone value 
+//undefined => (jab koi variable initialise nahi kara ho)
+
+
+//object
+
+console.log(typeof undefined)  //undefined
+console.log(typeof null)        //object
